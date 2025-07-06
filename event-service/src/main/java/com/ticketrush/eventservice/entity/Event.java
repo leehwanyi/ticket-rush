d70f382715bf507b35e07a3d19d9a7f61f4349fc
hwanyi <leehwanyi84620@gmail.com>
2025-07-06 10:24:22 +0900

    event-service추가 및 종속성 설정 event 도메인 설정

---
+++ event-service/src/main/java/com/ticketrush/eventservice/entity/Event.java
@@ -0,0 +1,23 @@
+package com.ticketrush.eventservice.entity;
+
+import com.ticketrush.commoncore.entity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Event extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+
+}
